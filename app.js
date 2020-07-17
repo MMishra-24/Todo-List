565a7fc901d1c54369996f34f24e0ff9f1e7f00a
Your Name <you@example.com>
2020-07-17 10:06:55 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,162 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.set("view engine", "ejs");
+mongoose.connect("mongodb+srv://admin-mohit:test123@cluster0.4fsac.mongodb.net/todoListDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+const itemsSchema = {
+  name: String
+}
+const Item = mongoose.model("Item", itemsSchema);
+const item1 = new Item({
+  name: "Welcome to your todolist!"
+});
+const item2 = new Item({
+  name: "Hit the + button to add a new item."
+});
+const item3 = new Item({
+  name: "<--Hit this to delete an item."
+});
+const defaultItems = [item1, item2, item3];
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+}
+const List = mongoose.model("List",listSchema);
+app.get("/", function(req, res) {
+  Item.find({}, function(err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DB.");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItems
+      });
+    }
+  });
+});
+app.get("/:customListName",function(req,res){
+  const customListName = _.capitalize(req.params.customListName);
+  List.findOne({name:customListName},function(err,foundList){
+    if(!err){
+      if(!foundList){
+        //Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/"+customListName);
+      }else{
+        //show an existing list
+        res.render("list",{listTitle: foundList.name,newListItems: foundList.items});
+      }
+    }
+  });
+
+});
+
+
+app.post("/", function(req, res) {
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+  const item = new Item({
+    name: itemName
+  });
+  if(listName==="Today"){
+    item.save();
+    res.redirect("/");
+  }else{
+    List.findOne({name: listName},function(err,foundList){
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/"+listName);
+    });
+  }
+
+});
+app.post("/delete",function(req,res){
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+  if(listName === "Today"){
+    Item.findByIdAndRemove(checkedItemId,function(err){
+      if(!err){
+        //console.log("Successfully deleted checked item.");
+        res.redirect("/");
+      }
+    });
+  }else{
+    List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItemId}}},function(err,foundList){
+      if(!err){
+        res.redirect("/"+listName);
+      }
+    });
+  }
+
+
+});
+app.get("/about", function(req, res) {
+  res.render("about");
+})
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
+
+
+
+// var today=new Date();
+// var day="";
+// var currDay = today.getDay();
+// switch(currDay){
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//     day = "Tuesday"
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Friday";
+//     break;
+//   case 5:
+//     day = "Saturday";
+//     break;
+//   case 6:
+//     day = "Saturday";
+//     break;
+//   default:
+//     console.log("Error: current day is equal to "+currDay);
+// }
+// -----------------------------------------------------------------
+// const items=["Buy Food","Cook Food","Eat Food"];
+// var workItems=[];
+// const date = require(__dirname+"/date.js");
+// app.get("/",function(req,res){
+// let day = date.getDate();
+//   res.render("list",{
+//     listTitle: day,
+//     newListItems: items
+//   });
+// });
